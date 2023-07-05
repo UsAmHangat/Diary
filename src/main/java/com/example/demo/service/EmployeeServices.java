260ace63d8f580e33f77202b411436105560bab8
Joachim <Joachim.Nachbaur@vol.at>
2023-07-05 10:13:09 +0200

    Saftey first

---
+++ src/main/java/com/example/demo/service/EmployeeServices.java
@@ -0,0 +1,11 @@
+package com.example.demo.service;
+
+import com.example.demo.model.Employee;
+import java.util.List;
+
+public interface EmployeeServices {
+    List<Employee> getAllEmployee();
+    void save(Employee employee);
+    Employee getById(Long id);
+    void deleteViaId(long id);
+}
